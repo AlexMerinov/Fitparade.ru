3333c018c4df5324615e67ba873de0144b52e4ba
Konstantin <konstantin.kazan@gmail.com>
2023-10-03 18:16:54 +0300

    main slider

--- js/components/sliders/main.ts
+++ js/components/sliders/main.ts
@@ -1,18 +1,34 @@
 import Swiper from 'swiper';
-import { Navigation, Pagination, Parallax } from 'swiper/modules';
+import { Navigation, Pagination, Parallax, Autoplay } from 'swiper/modules';
+
+const fDataParallax = () => {
+    const mainSliderBg = document.querySelectorAll('.main-slider__bg');
+    mainSliderBg.forEach((item) => {
+        if (window.matchMedia('(max-width: 500px)').matches) {
+            item.setAttribute('data-swiper-parallax', '200');
+        } else {
+            item.setAttribute('data-swiper-parallax', '400');
+        }
+    });
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-    Swiper.use([Navigation, Pagination, Parallax]);
+    Swiper.use([Navigation, Pagination, Parallax, Autoplay]);
 
     const sliders = document.querySelectorAll('.js-slider-main');
 
     sliders.forEach((slider) => {
         const swiperSlder = new Swiper(slider, {
             slidesPerView: 'auto',
-            speed: 300,
+            speed: 700,
             parallax: true,
             loop: true,
             grabCursor: true,
+            autoplay: {
+                delay: 7000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            },
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
@@ -23,4 +39,10 @@ document.addEventListener('DOMContentLoaded', () => {
             },
         });
     });
+
+    fDataParallax();
+});
+
+window.addEventListener('resize', () => {
+    fDataParallax();
 });
