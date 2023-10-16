144aef100e3038083fb084893d7d2eb3a23e28ac
Konstantin <konstantin.kazan@gmail.com>
2023-10-16 10:48:45 +0300

    catalog filter adaptive

---
+++ js/components/catalog/filter.ts
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Кнопка корзины на мобиле
+
+    let winTop = 0;
+
+    document.body.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        if (
+            target.classList.contains('js-modal-filter-show') ||
+            target.closest('.js-modal-filter-show')
+        ) {
+            e.preventDefault();
+
+            winTop = window.scrollY;
+            document.body.classList.add('filter-modal-show');
+            document.body.style.top = `-${winTop}px`;
+            document.body.style.setProperty('--wintop', `${winTop}px`);
+
+            const modal = document.querySelector('.catalog-cols__filter');
+            modal?.setAttribute('aria-hidden', 'false');
+            modal?.classList.add('show');
+        }
+    });
+
+    document.body.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        if (
+            target.classList.contains('js-modal-filter-hide') ||
+            target.closest('.js-modal-filter-hide')
+        ) {
+            e.preventDefault();
+
+            document.body.classList.remove('filter-modal-show');
+            window.scroll(0, winTop);
+            document.body.style.removeProperty('top');
+            document.body.style.removeProperty('--wintop');
+            const modal = document.querySelector('.catalog-cols__filter');
+            modal?.setAttribute('aria-hidden', 'true');
+            setTimeout(() => {
+                modal?.classList.remove('show');
+            }, 300);
+        }
+    });
+});
--- js/components/catalog/index.ts
+++ js/components/catalog/index.ts
@@ -2,3 +2,4 @@ import './gallery';
 import './color';
 import './basket';
 import './load-more';
+import './filter';
